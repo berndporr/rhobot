db4a9aacf986e6ba218041b92424adb478dbaf16
JC Redmond <jcredmond1516@gmail.com>
2024-01-20 19:59:02 +0000

    gpio in test built w cmake

---
+++ tests/motor_test.cpp
@@ -0,0 +1,3 @@
+// from stationary, smooth to full speed clockwise, smooth to full speed counterclockwise
+// full speed clockwise, then full speed counterclockwise
+// do both of these for each motor individually and then both together
