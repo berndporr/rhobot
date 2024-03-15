e38c6fecd29284fadada8fd1be8eccaefaebcf0d
JC Redmond <jcredmond1516@gmail.com>
2024-03-15 14:56:23 +0000

    updated steering test

--- test_differential_steering.cpp
+++ test_differential_steering.cpp
@@ -5,23 +5,17 @@ int main(){
     RhoBot rhobot;
 
     rhobot.start();
-    
-    // move forward 30 cm
-    rhobot.moveInline(0.3, true);
 
-    // move backward 30 cm
-    rhobot.moveInline(0.3, false);
-
-    // move in a 20 cm square
-    for (int i = 0; i < 4; i++) {
-        rhobot.moveInline(0.2, true);
-        rhobot.changeHeading(90, true);
+    // move in a 15 cm octagon
+    for (int i = 0; i < 8; i++) {
+        rhobot.moveInline(0.15, true);
+        rhobot.changeHeading(45, true);
     }
 
-    // go backwards in a 20 cm square
-    for (int i = 0; i < 4; i++) {
-        rhobot.moveInline(0.2, false);
-        rhobot.changeHeading(90, false);
+    // go backwards in a 15 cm octagon
+    for (int i = 0; i < 8; i++) {
+        rhobot.changeHeading(45, false);
+        rhobot.moveInline(0.15, false);
     }
 
     rhobot.stop();
